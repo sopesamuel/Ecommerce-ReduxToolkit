86d4e98c961e7e63a7a12e60e26ee563a02d8925
sopesamuel <fasusimosope@gmail.com>
2022-12-13 19:21:49 +0100

    first commit

---
+++ src/components/CartContainer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useDispatch ,useSelector } from 'react-redux';
+import CartItem from './CartItem';
+import { openModal } from '../Features/Modal/ModalSlice';
+ 
+export default function CartContainer() {
+   const dispatch = useDispatch();
+   const {cartItems, total, amount} = useSelector((store) => store.cart)
+
+   if(amount < 1){
+    return (
+    <section className='cart'>
+        <header>
+            <h2>Your Bag</h2>
+            <h4 className='empty-cart'>Is Currently Empty</h4>
+        </header>
+    </section>
+    );
+   }
+  return  <section className='cart'>
+    <header>
+        <h2>Your bag</h2>
+    </header>
+    <div>
+        {cartItems.map((item) =>{
+        return <CartItem key={item.id} {...item}/>
+        })}
+    </div>
+    <footer>
+        <hr />
+    <div className='cart-total'>
+        <h4>
+            Total <span>${total.toFixed(2)}</span>
+        </h4>
+    </div>
+    <button className='btn clear-btn' onClick={() => dispatch(openModal())}>Clear cart</button>
+    </footer>
+  </section>
+ 
+}
